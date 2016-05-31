c635a03a4b9a44f69cadcfcb7b7f1bba922e9eb5
Kiritoinder <inderbharaj@ymail.com>
2016-05-31 12:50:57 -0400

    Add project files.

---
+++ COMP213-S2016-lesson4/Person.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP213_S2016_lesson4
+{
+    /**
+     * This class defines a generic person
+     * 
+     * @class person
+     * @field {string} _name 
+     * @feild {int} _age
+     */
+    public class Person
+    {
+        //PRIVATE INSTANCE VARIABLES +++++++++++++++++++++++++
+        private string _name;
+        private int _age;
+
+        // PUBLIC PROPERTIES +++++++++++++++++++++++++++++++++
+
+        /**
+         * <summary>
+         * this is property for our private _name field
+         * </summary>
+         * 
+         * @property {string} Name
+         */
+
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+
+            set
+            {
+
+                this._name = value;
+            }
+        }
+        // CONSTRUCRORS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        /**
+         * <summary 
+         * This is a public property for our private _age feild
+         * </summary>
+         * 
+         * @property {int} Age
+         */
+        public int Age
+        {
+            get
+            {
+                return this._age;
+            }
+            set
+            {
+                this._age = value;
+            }
+
+        }
+
+        /**
+         * This is a default empty constructor for the person class
+         * 
+         * @constructor person
+         */
+
+
+
+        public Person()
+        {
+            //Initilize public properties
+            this.Name = "Unknown name";
+            this.Age = 0;
+        }
+
+        /**
+         * This is a constructor that takes name as as paramater and 
+         * passes it to the _name Private instance variable
+         * 
+         * @constructor person
+         * @parm {string} _name
+         */
+
+        public Person(string name)
+        {
+            this.Name = name;
+            this.Age = 0;
+        }
+
+        /**
+         * <summary>
+         * This is a constructor that takes age as paramater and passes
+         * it to the _age private instance variable
+         * </summary
+         * 
+         *
+         */
+        public Person(int age)
+        {
+            this.Name = "unknown name!";
+            this.Age = age;
+        }
+
+        /**<summary>
+         *This is a constructor that takes both name and age as paramaters,
+         *It assigns the local name valur to the Name property
+         * It assigns the local age valur to the Age property
+         * </summary>
+         * 
+         * @constructor Person
+         * 
+         */
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public void _sayHello()
+        {
+            Console.WriteLine(this.Name + " Says hello!");
+        }
+
+        /**
+         * 
+         * This method outputs the _name and _age values in the following
+         * format: _name + "is" +_age + "years old".
+         * 
+         * @method ShowAge
+         */
+        public void ShowAge()
+        {
+            Console.WriteLine(this.Name + " is " + this.Age + " years old");
+        }
+    }
+}
---
+++ COMP213-S2016-lesson4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP213_S2016_lesson4
+{
+    /**
+     * this class is the "driver" class for our program
+     */
+    public class Program
+    {
+        /**
+         * the main method for driver class
+         * 
+         * @method main
+         * @parm {string[]} args
+         */
+        public static void Main(string[] args)
+        {
+            // Create a new instance(object) of the person class
+
+            Person Abhinav = new Person();
+            Abhinav.Name = "Tanya";
+            Abhinav.Age = 28;
+            Abhinav._sayHello();
+            Abhinav.ShowAge();
+
+            Console.WriteLine();
+
+
+            Person Inder = new Person("Inder");
+            Inder.Age = 18;
+            Inder._sayHello();
+            Inder.ShowAge();
+
+            Console.WriteLine();
+
+            Person Iqbal = new Person(30);
+            Iqbal.Name = "Iqbal";
+            Iqbal._sayHello();
+            Iqbal.ShowAge();
+
+            Console.WriteLine();
+
+            Person Khushi = new Person("Khusi", 23);
+
+            Khushi._sayHello();
+            Khushi.ShowAge();
+
+        }
+    }
+}
